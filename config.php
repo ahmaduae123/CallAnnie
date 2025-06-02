5f66349abd6d938da3fe69ef34f4246dfa68ecb7
ahmaduae123 <ahmadchatgptwala@gmail.com>
2025-06-02 19:09:45 +0500

    Create config.php

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+define('GOOGLE_API_KEY', 'AIzaSyCPIxC_AiktvBqWrKdwglCdUYMWiflxkHM'); // Your provided Google API key
+define('GOOGLE_PROJECT_ID', 'your-dialogflow-project-id'); // Replace with your Dialogflow Project ID
+?>
